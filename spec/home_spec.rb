cfce26949f1cda8ed791954eb63057ea3cf5678b
Phuc Trinh <trinhcanhphuc@gmail.com>
2020-07-22 09:58:10 +0700

    add selenium test with rspec

---
+++ spec/home_spec.rb
@@ -0,0 +1,11 @@
+# spec/features/create_user.rb
+require 'rails_helper'
+
+describe 'check home page', type: :feature, js: true do
+  it 'successfully go home page' do
+    visit '/welcome/index'
+
+    expect(page).to have_content 'Welcome#index'
+    expect(page).to have_content 'Find me in app/views/welcome/index.html.erb'
+  end
+end
